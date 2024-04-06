1b4b43ed87fd21046e27781629d74eff7b8d2c0c
Ritesh Kumar Singh <110715583+Ritesh1912@users.noreply.github.com>
2024-04-06 10:22:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import { Route, Router, Routes, BrowserRouter  } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import SearchPage from './pages/SearchPage/SearchPage';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/search" element={<SearchPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
